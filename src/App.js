d89886f32977d24f4220132e3a34896e18c4608f
Your Name <jgbharris@hotmail.co.uk>
2021-02-15 02:46:23 +0000

    data now pulling from forecast API

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ const api = {
 function App() {
   const dateBuild = (d) => {
     let date = String(new window.Date());
-    date = date.slice(3, 15);
+    console.log("date 1", date)
+    date = date.slice(0, 21);
+    console.log("date 2", date)
     return date;
   };
 
@@ -17,13 +19,16 @@ function App() {
   const [weather, setWeather] = useState({});
   const search = (e) => {
     if (e.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      // fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       // fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${api.key}`)
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&units=metric&APPID=${api.key}`)
+      // fetch(`http://pro.openweathermap.org/data/2.5/forecast/hourly?q=London,us&mode=xml&appid=${api.key}`)
         .then((res) => res.json())
         .then((result) => {
           setQuery("");
           setWeather(result);
           console.log(result);
+          // console.log(result.list[0])
         });
     }
   };
@@ -52,19 +57,21 @@ function App() {
             />
           </label>
         </div>
-        {typeof weather.main != "undefined" ? (
+        {typeof weather != "undefined" && typeof weather.list != "undefined" ? (
           <div>
             <div className="location-container">
               <div className="location">
-                {weather.name}, {weather.sys.country}
+                {/* {weather.city.name}, {weather.city.country} */}
               </div>
               <div className="date"> {dateBuild(new Date())}</div>
             </div>
             <div className="weather-container">
               <div className="temperature">
-                {Math.round(weather.main.temp)}°C
+                {Math.round(weather.list[0]["main"]["temp"])}°C
               </div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="weather">{(weather.list[0]["main"]["feels_like"])}</div>
+              <div className="weather">{(weather.list[0]["main"]["humidity"])}</div>
+              <div className="weather">{(weather.list[0]["main"]["description"])}</div>
             </div>
           </div>
         ) : (
